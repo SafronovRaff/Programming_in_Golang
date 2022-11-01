9a78b4ed662b6fdd6601d7f645e41f79ba6779af
Roman <safronovraff@gmail.com>
2022-11-01 19:20:23 +0300

    String

--- 2 block/2.4_String/2.4.5.go
+++ 2 block/2.4_String/2.4.5.go
@@ -1,5 +1,30 @@
 package main
 
-/*
+import (
+	"bufio"
+	"fmt"
+	"os"
+	strings "strings"
+)
 
- */
+/*
+Дается строка. Нужно удалить все символы, которые встречаются более одного раза и вывести получившуюся строку
+
+Sample Input:
+
+zaabcbd
+Sample Output:
+
+zcd
+*/
+func main() {
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	var b = []rune{}
+	for _, i := range text {
+		if strings.Count(text, string(i)) == 1 { // Кол-во подстрок в строке strings.Count("test", "t"),  результат: 2
+			b = append(b, i)
+		}
+
+	}
+	fmt.Printf(string(b))
+}
--- 2 block/2.4_String/2.4.6.go
+++ 2 block/2.4_String/2.4.6.go
@@ -1,5 +1,30 @@
 package main
 
-/*
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
- */
+/*
+Дается строка. Нужно удалить все символы, которые встречаются более одного раза и вывести получившуюся строку
+
+Sample Input:
+
+zaabcbd
+Sample Output:
+
+zcd
+*/
+func main() {
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	var b = []rune{}
+	for _, i := range text {
+		if strings.Count(text, string(i)) == 1 {
+			b = append(b, i)
+		}
+
+	}
+	fmt.Printf(string(b))
+}
