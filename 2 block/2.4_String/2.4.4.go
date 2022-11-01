4a871cd103461346964b5e8ad79cc9a3c5fc97f6
Roman <safronovraff@gmail.com>
2022-11-01 17:11:38 +0300

    String

--- 2 block/2.4_String/2.4.4.go
+++ 2 block/2.4_String/2.4.4.go
@@ -1,5 +1,32 @@
 package main
 
-/*
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
- */
+/*
+На вход дается строка, из нее нужно сделать другую строку, оставив только нечетные символы (считая с нуля)
+
+Sample Input:
+
+ihgewlqlkot
+Sample Output:
+
+hello
+
+*/
+
+func main() {
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	t := []byte(text)
+	var a = []byte{}
+	for i, _ := range t {
+		if i%2 != 0 {
+			a = append(a, t[i])
+		}
+	}
+	fmt.Printf("%s", a)
+
+}
