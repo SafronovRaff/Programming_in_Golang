671531f3b1aeac3431607e3d61e8862a996febca
Roman W10 <Safronovraff@gmail.com>
2022-11-16 21:01:07 +0300

    path и path/filepath

---
+++ 3 block/3.5_Working with files/3.5.3.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func walkFunc(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err // Если по какой-то причине мы получили ошибку, проигнорируем эту итерацию
+	}
+
+	if info.IsDir() {
+		return nil // Проигнорируем директории
+	}
+
+	fmt.Printf("Name: %s\tSize: %d byte\tPath: %s\n", info.Name(), info.Size(), path)
+	return nil
+}
+
+func main() {
+	const root = "C:/Users/R/GolandProjects/Programming_in_Golang" // Файлы моей программы находятся в другой директории
+
+	if err := filepath.Walk(root, walkFunc); err != nil {
+		fmt.Printf("Какая-то ошибка: %v\n", err)
+	}
+
+}
